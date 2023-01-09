1c9ca106c4feafec0a15024a14cf341432d29174
Noel De Martin <noel@moodle.com>
2023-01-09 15:34:17 +0100

    Improve HTML diffs

--- classes/snapshots/html_snapshot.php
+++ classes/snapshots/html_snapshot.php
@@ -7,25 +7,38 @@ class html_snapshot extends behat_snapshot {
     protected $extension = 'html';
 
     public function diff(): string {
-        $this->store_failure_diffs();
-
+        $failuresdirectory = $this->store_failure_diffs();
         $storedhtml = $this->get_stored_content();
         $currenthtml = $this->get_current_content();
         $storedhtmllines = explode("\n", $storedhtml);
         $currenthtmllines = explode("\n", $currenthtml);
+        $totallines = max(count($storedhtmllines), count($currenthtmllines));
+        $diffcounter = 0;
+        $diff = '';
 
-        return array_reduce(
-            range(0, count($currenthtmllines)),
-            function ($diff, $line) use ($storedhtmllines, $currenthtmllines) {
-                if ($currenthtmllines[$line] !== $storedhtmllines[$line]) {
-                    $diff .= "before: $storedhtmllines[$line]\n";
-                    $diff .= "   now: $currenthtmllines[$line]\n";
-                }
+        for ($line = 0; $line < $totallines; $line++) {
+            $storedline = $storedhtmllines[$line] ?? '';
+            $currentline = $currenthtmllines[$line] ?? '';
 
-                return $diff;
-            },
-            ''
-        );
+            if ($storedline === $currentline) {
+                continue;
+            }
+
+            $diffcounter++;
+
+            $diff .= "#$diffcounter\n";
+            $diff .= "< $storedline\n";
+            $diff .= "---\n";
+            $diff .= "> $currentline\n";
+        }
+
+        file_put_contents($this->get_file_path('', 'failures', 'diff'), $diff);
+
+        if ($diffcounter > 6 || strlen($diff) > 300) {
+            return "You can compare the differences looking at the files in $failuresdirectory.";
+        }
+
+        return $diff;
     }
 
     protected function load_content() {
